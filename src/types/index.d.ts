005f42b6a7983335b70fb87ff6dd1ee27aef9fea
BHyeonKim <rlaqhguse@gmail.com>
2023-09-12 00:56:38 +0900

    chore: 목업 데이터 추가

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+export type Data = { id: District; value_area: number; value_bar: number }
+
+export type District = '성북구' | '강남구' | '노원구' | '중랑구'
+
+export type Date = string
+
+export type MockData = {
+  type: District
+  version: number
+  response: {
+    [key: Date]: Data
+  }
+}
