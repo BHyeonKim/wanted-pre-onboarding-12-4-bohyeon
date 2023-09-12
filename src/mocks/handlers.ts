4c88366eafce8641f6379319a0393da90fa0b587
BHyeonKim <rlaqhguse@gmail.com>
2023-09-12 12:39:38 +0900

    chore: 안쓰는 코드 및 파일 제거

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,5 +3,5 @@ import { rest } from 'msw'
 import MOCK_DATA from './mock_data.json'
 
 export const handlers = [
-  rest.get('/data', (req, res, ctx) => res(ctx.status(200), ctx.json(MOCK_DATA))),
+  rest.get('/data', (_, res, ctx) => res(ctx.status(200), ctx.json(MOCK_DATA))),
 ]
