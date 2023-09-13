271c7a090b1a0bdf2be1812078b92ae5d7f77d3e
BHyeonKim <rlaqhguse@gmail.com>
2023-09-13 17:21:52 +0900

    fix: 배포환경에서도 api 모킹

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,11 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start()
+// }
+// 배포환경에서도 api 모킹 작동하게 설정
+worker.start()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
