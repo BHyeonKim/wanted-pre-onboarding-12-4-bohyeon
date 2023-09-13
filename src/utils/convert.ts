b1cf987837ca52f23a5d5532b1c1aef290c54301
BHyeonKim <rlaqhguse@gmail.com>
2023-09-13 17:20:40 +0900

    docs: 함수 주석 추가

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,6 +1,12 @@
 import { ConvertedData, MockData } from 'types'
 
-export const convertData = (data: MockData['response']) => {
+/**
+ * convertData 함수는 MockData 타입의 'response' 필드에서 데이터를 가져와서 차트에 맞게 정제된 데이터를 반환합니다.
+ *
+ * @param {Object} data - MockData 의 'response' 필드로, 변환할 데이터 객체입니다.
+ * @returns {ConvertedData[]} - 변환된 데이터 객체의 배열, 각 객체는 date, district, area 및 bar 속성을 포함합니다.
+ */
+export const convertData = (data: MockData['response']): ConvertedData[] => {
   const dataConvertedToArray: ConvertedData[] = []
 
   for (const date in data) {
@@ -17,12 +23,22 @@ export const convertData = (data: MockData['response']) => {
   return dataConvertedToArray
 }
 
+/**
+ *
+ * @param date - 날짜 문자열입니다.
+ * @returns - HH:MM:SS 형식의 시간 문자열을 반환합니다.
+ */
 export const convertDate = (date: string) => {
   const dateObj = new Date(date)
 
   return `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`
 }
 
+/**
+ * 정제된 데이터에서 지역구 리스트를 추출하는 함수입니다.
+ * @param  data - convertData함수의 리턴값입니다.
+ * @returns {string[]} - 지역구의 리스트입니다.
+ */
 export const getDistrictList = (
   data: ConvertedData[] | undefined,
 ): string[] | undefined => {
